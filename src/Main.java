ab73ba1c10415b594cf84da9ac35c75ffe72eb6f
aa.refandi <aa.refandi92@gmail.com>
2021-09-03 21:06:28 +0700

    test

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main (String args[]){
-        System.out.println("Hello World! Welcome to Koligrum Class!!");
+        System.out.println("Hello World! Welcome to Koligrum Class!");
         Other other = new Other();
         other.printNumber();
     }
