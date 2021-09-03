e5169e354c7b0f43608e00da931b5af3c78a87c1
aa.refandi <aa.refandi92@gmail.com>
2021-09-03 19:49:26 +0700

    add  2 class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main (String args[]){
         System.out.println("Hello World! Welcome to Koligrum Class!!");
+        Other other = new Other();
+        other.printNumber();
     }
 }
---
+++ src/Other.java
@@ -0,0 +1,9 @@
+public class Other {
+
+    public Other(){
+    }
+
+    public void printNumber(){
+        System.out.println("5");
+    }
+}
